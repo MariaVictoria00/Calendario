c7098a4eb4385b4fe530c34909cec1e59b6c655b
MariaVictoria00 <mariavictoriapaybe@gmail.com>
2021-03-05 14:47:04 -0400

    Ajustando bugs

--- main.js
+++ main.js
@@ -5,19 +5,28 @@ const calendario=()=>{
     //Inicializa mes 
     data.setDate(1);
 
-    //variaveis para controle de dia atual, anterior e futuro[]
-    const diasDoMes = document.querySelector('.dias'); 
-    const ultimoDia = new Date(data.getFullYear(),data.getMonth()+1,0).getDate();
-    const proxUltimoDia = new Date(data.getFullYear(),data.getMonth(),0).getDate();
-    const primeiroDia = data.getDay();
-    const proxUltimoDiaIndex = new Date(data.getFullYear(),data.getMonth()+1,0).getDay();
+    //variaveis para controle de dia atual, anterior e futuro
 
-    //ajustar
+    //determina a quantidade de dias para o mes 
+    const diasDoMes = document.querySelector('.dias'); 
+    
+    //retorna o ultimo dia do mes 
+    const ultimoDia = new Date(data.getFullYear(),data.getMonth()+1,0).getDate(); 
+
+    // retorna o ultimo dia do mes anterior
+    const proxUltimoDia = new Date(data.getFullYear(),data.getMonth(),0).getDate(); 
+
+    // retorna a posicao do  primeiro dia do mes 
+    const primeiroDia = data.getDay(); 
+
+    // retorna a posicao do  ultimo  dia do mes 
+    const proxUltimoDiaIndex = new Date(data.getFullYear(),data.getMonth()+1,0).getDay(); 
+    
+    //adicona no mes atual os dias do proximo mes 
     var  diasProximos = 0;
-    if(proxUltimoDia>32){
-         diasProximos =7-proxUltimoDiaIndex-1;
-    }else{
-        
+    if(primeiroDia>=5){
+         diasProximos =7-proxUltimoDiaIndex-1;    
+    }  else{   
         diasProximos =14-proxUltimoDiaIndex-1;
     }
     
@@ -38,20 +47,21 @@ const calendario=()=>{
         "Dezembro"
     ];
     
-    //controle da data atual no cabeçalho 
+    //Adiciona  o mes e o ano atual no cabeçalho 
     document.querySelector('.dia-atual h1').innerHTML = mes[data.getMonth()];
     document.querySelector('.dia-atual p').innerHTML =  data.getMonth()+1 + " | "+ data.getFullYear();
     
     let dias = "";
      
-    // controle do dia anterior 
+    // controle do ultimo dia do mes anterior
     for(let x=primeiroDia; x>0; x-- ){
         dias+= `<div class= "dia-anterior">${proxUltimoDia-x+1}</div>`
+        
         
     }
      
 
-    //controle do dia atual 
+    //controle do mes atual
     for(let i=1; i<=ultimoDia; i++){
         if(i== new Date().getDate() && data.getMonth()==new Date().getMonth() && data.getFullYear()==new Date().getFullYear() ){
             dias+= `<div class="hoje">${i}</div>`;
